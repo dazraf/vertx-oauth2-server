9dc1818811b1a244a67a2ca324428e2bc1cd247c
dazraf <farzad.pezeshkpour@gmail.com>
2016-03-24 01:49:50 +0000

    http utils rework

--- src/main/java/io/dazraf/oauth2/util/HttpUtils.java
+++ src/main/java/io/dazraf/oauth2/util/HttpUtils.java
@@ -2,12 +2,12 @@ package io.dazraf.oauth2.util;
 
 import io.vertx.ext.web.RoutingContext;
 
-public class HttpRequestUtils {
+public class HttpUtils {
   public static String checkPresent(RoutingContext context, String parameter) throws Exception {
     String value = context.request().getParam(parameter);
     if (value == null) {
       String msg = "the request is missing parameter: " + parameter;
-      badRequest(context, msg);
+      httpBadRequest(context, msg);
       throw new Exception(msg);
     }
     return value;
@@ -17,13 +17,18 @@ public class HttpRequestUtils {
     String value = context.request().getParam(parameter);
     if (value == null || !value.equals(expectedValue)) {
       String msg =  "the request parameter: " + parameter + " should be " + expectedValue;
-      badRequest(context, msg);
+      httpBadRequest(context, msg);
       throw new Exception(msg);
     }
     return value;
   }
 
-  public static void badRequest(RoutingContext context, String errorMessage) {
+  public static void httpBadRequest(RoutingContext context, String errorMessage) {
     context.response().setStatusCode(400).setStatusMessage(errorMessage).end();
   }
+
+  public static void httpRedirectTemporary(RoutingContext context, String redirectURI) {
+    context.response().setStatusCode(303).putHeader("Location", redirectURI).end();
+  }
+
 }
